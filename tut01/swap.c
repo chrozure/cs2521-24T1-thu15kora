85cefbae0ed8784b556778d4498efc92de124e78
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-02-14 19:44:32 +1100

    Created folder for tut01

---
+++ tut01/swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void) {
+	int a = 5;
+	int b = 7;
+	swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+
+	printf("%d\n", a);
+}
+
+void swap(int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
