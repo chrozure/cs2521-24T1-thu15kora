cafdadbdf7a17f86b662036c214e509062246d18
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-04-03 20:09:39 +1100

    code ready for tut08

---
+++ tut08/traversal/reachable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct set *Set;
+typedef struct graph *Graph;
+typedef struct vertex *Vertex;
+
+Set reachable(Graph g, Vertex src);
+
+int main(void) {
+
+}
+
+
+Set reachable(Graph g, Vertex src) {
+
+    return NULL;
+};
