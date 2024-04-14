fd67db920ff38aeb9a42d164cb052fe4afe07ce9
Harry Zhang <z5363493@ad.unsw.edu.au>
2024-04-14 15:26:29 +1000

    added tut09 code

--- tut09/hashTables/simpleHash.c
+++ tut09/hashTables/simpleHash.c
@@ -5,19 +5,22 @@ int hash(char *key, int N);
 int main(void) {
 
     int size = 10;
-    printf("hash(\"hi\") = %d\n", hash("hi", size));
+    // printf("hash(\"hi\") = %d\n", hash("hi", size));
     printf("hash(\"cat\") = %d\n", hash("cat", size));
-    printf("hash(\"hello\") = %d\n", hash("hello", size));
-    printf("hash(\"zID\") = %d\n", hash("zID", size));
+    printf("hash(\"tac\") = %d\n", hash("tac", size));
+    printf("hash(\"act\") = %d\n", hash("act", size));
+    // printf("hash(\"hello\") = %d\n", hash("hello", size));
+    // printf("hash(\"zID\") = %d\n", hash("zID", size));
 
     return 0;
 }
 
 int hash(char *key, int N) {
 	int h = 0;
+    int position = 0;
 	char *c;
-	for (c = key; *c != '\0'; c++) {
-		h = h + *c;
+	for (c = key; *c != '\0'; c++, position++) {
+		h = h + (*c * position);
 	}
 	return h % N;
 }
